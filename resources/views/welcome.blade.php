e023a508635767791f80672f3dffe010d7d4b2e3
Ananth Sathvick <ananthsathvick@gmail.com>
2019-03-21 12:25:53 +0530

    INstalled laravel telescope and added bootstrap grayscale template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,326 +1,227 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<html lang="en">
 
-        <title>Aegle Hospitals</title>
+<head>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
-        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-        <!-- Styleskk -->
-        <style>
-            html, body {
-               
-                /* color: #636b6f; */
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100%;
-                margin: 0;
-                /* display: inline; */
-            }
-            html
-            {
-                display:inline;
-            }
-        
-            
-            .full-height {
-                height: 100vh;
-            }
+  <title>Grayscale - Start Bootstrap Theme</title>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-            .position-ref {
-                position: relative;
-            }
+  <!-- Custom fonts for this template -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+  <!-- Custom styles for this template -->
+  <link href="css/grayscale.css" rel="stylesheet">
 
-            .content {
-                text-align: center;
-            }
+</head>
 
-            .title {
-                margin:0;
-                font-size: 84px;
-                color: white;
-  text-shadow: 2px 2px 4px black;
-                text-align:center;
-                position:absolute;
-                top:50%;
-                left:50%;
-                /* -ms-transform:translate(-50%,-50%); */
-                transform:translate(-50%,60%);
-                
-            }
-            .sub {
-                margin:auto;
-                font-size: 20px;
-                color: white;
-  text-shadow: 2px 2px 4px black;
-                text-align:center;
-                position:relative;
-                padding:10% 0 0;
-            }
+<body id="page-top">
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">Hospitals</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">Log in</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#projects">Register</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#signup">Dr.Login</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-            .navigation {
-    
-                display: inline-block;
-                float: right;
-                /* padding: 2% 2% 2% 2%;
-                padding-left: 5%; */
-                background:black;
-                color: white;
-                position: sticky;
-                top: 0;
-                width: 100%;
-                margin:0;
-                opacity:0.75;
-                z-index: 10;
-                border: right 10;  
-               }
-               .heade{
-                   text-align:center;
-                   position:absolute;
-                   top:100%;
-                   width:100%;
-                   color:#636b6f;
-                   background:black;
-                   
+  <!-- Header -->
+  <header class="masthead">
+    <div class="container d-flex h-100 align-items-center">
+      <div class="mx-auto text-center">
+        <h1 class="mx-auto my-0 text-uppercase">Hospitals</h1>
+        <h2 class="text-white-50 mx-auto mt-2 mb-5">Where healthcare comes first</h2>
+        <a href="#about" class="btn btn-primary js-scroll-trigger">Get Started</a>
+      </div>
+    </div>
+  </header>
 
-               }
-               .serv{
-                   color:white;
-                   font-size:20px;
-               }
+  <!-- About Section -->
+  <section id="about" class="about-section text-center">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2 class="text-white mb-4">Built with Bootstrap 4</h2>
+          <p class="text-white-50">Grayscale is a free Bootstrap theme created by Start Bootstrap. It can be yours right now, simply download the template on
+            <a href="http://startbootstrap.com/template-overviews/grayscale/">the preview page</a>. The theme is open source, and you can use it for any purpose, personal or commercial.</p>
+        </div>
+      </div>
+      <img src="img/ipad.png" class="img-fluid" alt="">
+    </div>
+  </section>
 
-               .sidenav {
-  height: 100%;
-  width: 0;
-  position: fixed;
-  z-index: 1;
-  top: 0;
-  left: 0;
-  background-color: #111;
-  overflow-x: hidden;
-  transition: 0.5s;
-  padding-top: 60px;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-}
-.sidenav a {
-  padding: 8px 8px 8px 32px;
-  text-decoration: none;
-  font-size: 20px;
-  color: #818181;
-  display: block;
-  transition: 0.3s;
-}
+  <!-- Projects Section -->
+  <section id="projects" class="projects-section bg-light">
+    <div class="container">
 
-.sidenav a:hover {
-  color: #f1f1f1;
-}
+      <!-- Featured Project Row -->
+      <div class="row align-items-center no-gutters mb-4 mb-lg-5">
+        <div class="col-xl-8 col-lg-7">
+          <img class="img-fluid mb-3 mb-lg-0" src="img/bg-masthead.jpg" alt="">
+        </div>
+        <div class="col-xl-4 col-lg-5">
+          <div class="featured-text text-center text-lg-left">
+            <h4>Shoreline</h4>
+            <p class="text-black-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it for any project - even commercial projects! Download it, customize it, and publish your website!</p>
+          </div>
+        </div>
+      </div>
 
-.sidenav .closebtn {
-  position: absolute;
-  bottom: 0;
-  right: 25px;
-  font-size: 36px;
-  margin-left: 50px;
-}
-
-@media screen and (max-height: 450px) {
-  .sidenav {padding-top: 15px;}
-  .sidenav a {font-size: 18px;}
-}
-
-               .nav-content {
-                        display: inline;
-                        position: sticky;
-                        font-size: 1.4rem;
-                        margin: 1% 0% 1% 0%;
-                        float :right;
-                            }
-        .nav-content a {
-                text-decoration: whitesmoke;
-                color: white;
-                
-                         }
-  .nav-content a:hover{
-      text-decoration:none;
-      color:grey;
-                }
-  .nav-link:focus,.nav-link:hover{
-    text-decoration:none;
-  color:grey;}
-            body  {
-        background-image:url("img/hush.jpg");
-        background-repeat: no-repeat;
-        background-size:cover;
-        background-position: center;
-        background-attachment:fixed;
-        height:100%;
-        position:relative;
-       }
-       @media only screen and (max-device-width: 1024px) {
-  body {
-    background-attachment: scroll;
-  }
-}
-@media only screen and (max-device-width: 425px){
-    .nav-content{
-        align:center;
-        font-size:2px;
-        margin:1% 0% 1% 0%;
-        padding:0px 0px;
-    }
-    .links > a {
-        padding:0px 10px;
-        text-align:center;
-    }
-    .bars {
-        font-size:20px;
-    }
-    .title {
-        font-size:60px;
-    }
-    .sub{
-        padding:0 0 0 0;
-        transform:translate(0%,60%);
-    }
-   
-}
-        </style>
-  </head>
-            <body >
-  
-    <!-- <div class="navigation"> -->
-      <!-- <div class="nav-content"> -->
-        <div class="flex-center position-ref navigation">
-            @if (Route::has('login'))
-            <span style="font-size:30px;cursor:pointer" class="bars" onclick="openNav(x)">&nbsp &#9776; </span>
-                <div class="top-right links nav-content">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                    @auth('admin')
-                        <a href="{{ url('/admin') }}">Home</a>
-                        @else
-                        <a href="{{ route('admin.login') }}">Dr.Login</a>
-                    @endauth
-                </div>
-            @endif
+      <!-- Project One Row -->
+      <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+        <div class="col-lg-6">
+          <img class="img-fluid" src="img/demo-image-01.jpg" alt="">
+        </div>
+        <div class="col-lg-6">
+          <div class="bg-black text-center h-100 project">
+            <div class="d-flex h-100">
+              <div class="project-text w-100 my-auto text-center text-lg-left">
+                <h4 class="text-white">Misty</h4>
+                <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else, along with a description.</p>
+                <hr class="d-none d-lg-block mb-0 ml-0">
+              </div>
             </div>
-           
-             
-            <script>
-AOS.init();
+          </div>
+        </div>
+      </div>
 
-// function Nav()
-//  {
-//      if(document.getElementById("mySidenav").style.width == "0")
-//         return(openNav(x));
-//      else 
-//         return(closeNav(x));   
-//  }
+      <!-- Project Two Row -->
+      <div class="row justify-content-center no-gutters">
+        <div class="col-lg-6">
+          <img class="img-fluid" src="img/demo-image-02.jpg" alt="">
+        </div>
+        <div class="col-lg-6 order-lg-first">
+          <div class="bg-black text-center h-100 project">
+            <div class="d-flex h-100">
+              <div class="project-text w-100 my-auto text-center text-lg-right">
+                <h4 class="text-white">Mountains</h4>
+                <p class="mb-0 text-white-50">Another example of a project with its respective description. These sections work well responsively as well, try this theme on a small screen!</p>
+                <hr class="d-none d-lg-block mb-0 mr-0">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
-function openNav(x) {
-    if(x.matches){
-        document.getElementById("mySidenav").style.width = "250px";
-        // document.getElementByClass("navigation").style.opacity="1";
-    }
-    else{
-        document.getElementById("mySidenav").style.width = "100%";
-        // document.getElementByClass("navigation").style.opacity="0";
-    }
-}
+    </div>
+  </section>
 
-function closeNav(x) {
-    if(x.matches){
-  document.getElementById("mySidenav").style.width = "0";
-}
-else{
-    document.getElementById("mySidenav").style.width = "0";
-}}
+  <!-- Signup Section -->
+  <section id="signup" class="signup-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-10 col-lg-8 mx-auto text-center">
 
-var x = window.matchMedia("(min-width: 700px)")
-x.addListener(openNav)
-x.addListener(closeNav)
-</script>
+          <i class="far fa-paper-plane fa-2x mb-2 text-white"></i>
+          <h2 class="text-white mb-5">Subscribe to receive updates!</h2>
 
-            <div class="bg"></div>
-            <div id="mySidenav" class="sidenav">
-            <!-- <a href="javascript:void(0)" class="closebar" onclick="closeNav(x)">&#9776;</a> -->
-  <a href="javascript:void(0)" class="closebtn" onclick="closeNav(x)">&times;</a>
-  <a href="#about" onclick="closeNav(x)" >About</a>
-  <a href="#services" onclick="closeNav(x)">Services</a>
-  <a href="#contact" onclick="closeNav(x)">Contact</a>
-</div>
-<section>
-<div data-aos="fade-up"
-     data-aos-duration="3000"><div class="title"> Welcome to Aegle Hospitals</div>
-     <div class="sub">WHERE TECHNOLOGY MEETS HEALTHCARE</div>
-</div>
+          <form class="form-inline d-flex">
+            <input type="email" class="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0" id="inputEmail" placeholder="Enter email address...">
+            <button type="submit" class="btn btn-primary mx-auto">Subscribe</button>
+          </form>
 
-  </section>     
-  <section data-aos="fade-up"
-  data-aos-anchor-placement="top-center" class="heade" name="services">
-    <div id="services" class="serv">Services Offered</div>
-       <ul type="none"> 
-       <li>short-term hospitalization</li>
-<li>emergency room services</li>
-<li>general and specialty surgical services</li>
-<li>x ray/radiology services</li>
-<li>laboratory services</li>
-<li>blood services</li>
-<li>Pediatric specialty care</li>
-<li>Physical therapy and rehabilitation services</li>
-<li>Home nursing services</li>
-<li>Nutritional counseling</li>
+        </div>
+      </div>
+    </div>
+  </section>
 
+  <!-- Contact Section -->
+  <section class="contact-section bg-black">
+    <div class="container">
 
+      <div class="row">
 
+        <div class="col-md-4 mb-3 mb-md-0">
+          <div class="card py-4 h-100">
+            <div class="card-body text-center">
+              <i class="fas fa-map-marked-alt text-primary mb-2"></i>
+              <h4 class="text-uppercase m-0">Address</h4>
+              <hr class="my-4">
+              <div class="small text-black-50">4923 Market Street, Orlando FL</div>
+            </div>
+          </div>
+        </div>
 
-       
-       
-       </ul>
+        <div class="col-md-4 mb-3 mb-md-0">
+          <div class="card py-4 h-100">
+            <div class="card-body text-center">
+              <i class="fas fa-envelope text-primary mb-2"></i>
+              <h4 class="text-uppercase m-0">Email</h4>
+              <hr class="my-4">
+              <div class="small text-black-50">
+                <a href="#">hello@yourdomain.com</a>
+              </div>
+            </div>
+          </div>
+        </div>
 
-</section>
+        <div class="col-md-4 mb-3 mb-md-0">
+          <div class="card py-4 h-100">
+            <div class="card-body text-center">
+              <i class="fas fa-mobile-alt text-primary mb-2"></i>
+              <h4 class="text-uppercase m-0">Phone</h4>
+              <hr class="my-4">
+              <div class="small text-black-50">+1 (555) 902-8832</div>
+            </div>
+          </div>
+        </div>
+      </div>
 
-           </body>
-         <footer>
-         <!-- <i class="fas fa-ellipsis-v"></i> -->
-         </footer>
+      <div class="social d-flex justify-content-center">
+        <a href="#" class="mx-2">
+          <i class="fab fa-twitter"></i>
+        </a>
+        <a href="#" class="mx-2">
+          <i class="fab fa-facebook-f"></i>
+        </a>
+        <a href="#" class="mx-2">
+          <i class="fab fa-github"></i>
+        </a>
+      </div>
+
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="bg-black small text-center text-white-50">
+    <div class="container">
+      Copyright &copy; Your Website 2019
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/grayscale.min.js"></script>
+
+</body>
+
 </html>
